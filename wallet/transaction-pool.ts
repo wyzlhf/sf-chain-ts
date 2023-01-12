d522a515f5a47b292d054fe3558253c98562db09
wyzlhf <haifeng0130@126.com>
2023-01-12 13:42:35 +0800

    重新整理了Block中的data数据结构。讲他的unittest去掉了，用js写的测试，在ts看来就是在胡测呢。

--- wallet/transaction-pool.ts
+++ wallet/transaction-pool.ts
@@ -1,40 +1,43 @@
 import {Transaction} from "./transaction";
 
-export class TransactionPool{
-    transactions:Transaction[]
+export class TransactionPool {
+    transactions: Transaction[]
+
     constructor() {
-        this.transactions=[]
+        this.transactions = []
     }
-    public updateOrAddTransaction(transaction:Transaction):void{
-        let transactionWitId=this.transactions.find(t=>t.id===transaction.id)
-        if(transactionWitId){
-            this.transactions[this.transactions.indexOf(transactionWitId)]=transaction
-        }else {
+
+    public updateOrAddTransaction(transaction: Transaction): void {
+        let transactionWitId = this.transactions.find(t => t.id === transaction.id)
+        if (transactionWitId) {
+            this.transactions[this.transactions.indexOf(transactionWitId)] = transaction
+        } else {
             this.transactions.push(transaction)
         }
     }
 
-    public existingTransaction(publicKey: string):Transaction {
-        return this.transactions.find(t=>t.input?.address===publicKey) as Transaction
+    public existingTransaction(publicKey: string): Transaction {
+        return this.transactions.find(t => t.input?.address === publicKey) as Transaction
     }
 
-    public validTransactions():Transaction[] {
-        return this.transactions.filter(transaction=>{
-            const outputTotal=transaction.outputs.reduce((total,output)=>{
-                return total+output.amount
-            },0)
-            if (transaction.input?.amount!==outputTotal){
+    public validTransactions(): Transaction[] {
+        return this.transactions.filter(transaction => {
+            const outputTotal = transaction.outputs.reduce((total, output) => {
+                return total + output.amount
+            }, 0)
+            if (transaction.input?.amount !== outputTotal) {
                 console.log(`Invalid transaction from ${transaction.input?.address}`)
                 return
             }
-            if(!Transaction.verifyTransaction(transaction)){
+            if (!Transaction.verifyTransaction(transaction)) {
                 console.log(`Invalid signature from ${transaction.input.address}`)
                 return;
             }
             return transaction
         })
     }
-    public clear():void{
-        this.transactions=[]
+
+    public clear(): void {
+        this.transactions = []
     }
 }
