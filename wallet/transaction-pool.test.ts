c596f1fc591fb05bc7c304bdeab30caeb622de19
wyzlhf <haifeng0130@126.com>
2023-01-12 09:05:21 +0800

    添加transaction

--- wallet/transaction-pool.test.ts
+++ wallet/transaction-pool.test.ts
@@ -8,8 +8,9 @@ describe('TransactionPool',()=>{
     beforeEach(()=>{
         tp=new TransactionPool()
         wallet=new Wallet()
-        transaction=Transaction.newTransaction(wallet,'r4nd-4dr455',30) as Transaction
-        tp.updateOrAddTransaction(transaction)
+        // transaction=Transaction.newTransaction(wallet,'r4nd-4dr455',30) as Transaction
+        // tp.updateOrAddTransaction(transaction)
+        transaction=wallet.createTransaction('r4nd-4dr455',30,tp) as Transaction
     })
     it('should add a transaction to the pool', function () {
         expect(tp.transactions.find(t=>t.id===transaction.id)).toEqual(transaction)
@@ -20,4 +21,26 @@ describe('TransactionPool',()=>{
         tp.updateOrAddTransaction(newTransaction)
         expect(JSON.stringify(tp.transactions.find(t=>t.id===newTransaction.id))).not.toEqual(oldTransaction)
     });
+
+    describe('mixing valid and corrupt transactions',()=>{
+        let validTransactions:Transaction[]
+        beforeEach(()=>{
+            validTransactions=[...tp.transactions]
+            for(let i:number=0;i<6;i++){
+                wallet=new Wallet()
+                transaction=wallet.createTransaction('r4nd-4dr355',30,tp) as Transaction
+                if(i%2===0){
+                    transaction.input!.amount=99999
+                }else {
+                    validTransactions.push(transaction)
+                }
+            }
+        })
+        it('should show a difference between valid and corrupt transactions', function () {
+            expect(JSON.stringify(tp.transactions)).not.toEqual(JSON.stringify(validTransactions))
+        });
+        it('should grab valid transactions', function () {
+            expect(tp.validTransactions()).toEqual(validTransactions)
+        });
+    })
 })
