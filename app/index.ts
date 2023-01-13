4693c897050ac2c674c1bd4deaafb852fc9e7184
wyzlhf <haifeng0130@126.com>
2023-01-13 19:38:36 +0800

    重新整理了Block中的data数据结构。将他的unittest去掉了，用js写的测试，在ts看来就是在胡测呢。

--- app/index.ts
+++ app/index.ts
@@ -7,46 +7,46 @@ import {TransactionPool} from "../wallet/transaction-pool";
 import {Transaction} from "../wallet/transaction";
 import {Miner} from "./miner";
 
-const HTTP_PORT=process.env.HTTP_PORT||3001
+const HTTP_PORT = process.env.HTTP_PORT || 3001
 
-const app=express()
-const bc=new Blockchain()
-const wallet=new Wallet()
-const tp=new TransactionPool()
-const p2pServer=new P2pServer(bc,tp)
-const miner=new Miner(bc,tp,wallet,p2pServer)
+const app = express()
+const bc = new Blockchain()
+const wallet = new Wallet()
+const tp = new TransactionPool()
+const p2pServer = new P2pServer(bc, tp)
+const miner = new Miner(bc, tp, wallet, p2pServer)
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({extended: false}))
 
-app.get('/blocks', (req,res)=>{
+app.get('/blocks', (req, res) => {
     res.json(bc.chain)
 })
-app.post('/mine',(req,res)=>{
-    const block=bc.addBlock(req.body.data)
+app.post('/mine', (req, res) => {
+    const block = bc.addBlock(req.body.data)
     console.log(`New block added:${block.toString()}`)
     p2pServer.syncChains()
     res.redirect('/blocks')
 })
-app.get('/transactions',(req,res)=>{
+app.get('/transactions', (req, res) => {
     res.json(tp.transactions)
 })
-app.post('/transaction',(req,res)=>{
-    const {recipient,amount}=req.body
-    const transaction=wallet.createTransaction(recipient,amount,bc,tp)
+app.post('/transaction', (req, res) => {
+    const {recipient, amount} = req.body
+    const transaction = wallet.createTransaction(recipient, amount, bc, tp)
     p2pServer.broadcastTransaction(transaction as Transaction)
     res.redirect('/transactions')
 })
-app.get('/public-key',(req,res)=>{
-    res.json({publicKey:wallet.publicKey})
+app.get('/public-key', (req, res) => {
+    res.json({publicKey: wallet.publicKey})
 })
-app.get('/mine-transactions',(req,res)=>{
-    const block=miner.mine()
+app.get('/mine-transactions', (req, res) => {
+    const block = miner.mine()
     console.log(`New block added:${block.toString()}`)
     res.redirect('/blocks')
 })
 
-app.listen(HTTP_PORT,():void=>{
+app.listen(HTTP_PORT, (): void => {
     console.log(`Listening on port ${HTTP_PORT}`)
 })
 p2pServer.listen()
--- app/miner.ts
+++ app/miner.ts
@@ -5,23 +5,25 @@ import {P2pServer} from "./p2p-server";
 import {Transaction} from "../wallet/transaction";
 import {Block} from "../blockchain/block";
 
-export class Miner{
-    blockchain:Blockchain
-    transactionPool:TransactionPool
-    wallet:Wallet
-    p2pServer:P2pServer
-    constructor(blockchain:Blockchain,transactionPool:TransactionPool,wallet:Wallet,p2pServer:P2pServer) {
-        this.blockchain=blockchain
-        this.transactionPool=transactionPool
-        this.wallet=wallet
-        this.p2pServer=p2pServer
+export class Miner {
+    blockchain: Blockchain
+    transactionPool: TransactionPool
+    wallet: Wallet
+    p2pServer: P2pServer
+
+    constructor(blockchain: Blockchain, transactionPool: TransactionPool, wallet: Wallet, p2pServer: P2pServer) {
+        this.blockchain = blockchain
+        this.transactionPool = transactionPool
+        this.wallet = wallet
+        this.p2pServer = p2pServer
     }
-    public mine():Block{
-        const validTransactions:Transaction[]=this.transactionPool.validTransactions()
+
+    public mine(): Block {
+        const validTransactions: Transaction[] = this.transactionPool.validTransactions()
         validTransactions.push(
-            Transaction.rewardTransaction(this.wallet,Wallet.blockchainWallet())
+            Transaction.rewardTransaction(this.wallet, Wallet.blockchainWallet())
         )
-        const block=this.blockchain.addBlock(validTransactions)
+        const block = this.blockchain.addBlock(validTransactions)
         this.p2pServer.syncChains()
         this.transactionPool.clear()
         this.p2pServer.broadcastClearTransactions()
